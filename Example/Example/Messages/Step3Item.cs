d1013e4dadae4be6a6b21f470b4d281197cc1db9
JA Reyes <jarDotNet@users.noreply.github.com>
2015-12-23 15:37:38 +0100

    Review

--- Example/Example/Messages/Step3Item.cs
+++ Example/Example/Messages/Step3Item.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Example.Messages
 {
     class Step3Item
     {
-        public Step3Item(int value)
+        public Step3Item(int value, IEnumerable<int> items)
         {
             Value = value;
+            Items = items.ToArray();
         }
 
         public int Value { get; private set; }
+        public IEnumerable<int> Items { get; private set; }
     }
 }
